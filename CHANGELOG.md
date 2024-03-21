d6f9d7d423f86b8f30dbc7df4bd767ddaf846708
awaisfusion <muhammadawais@fusionwave.co>
2024-03-21 10:30:26 +0500

    intial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.0.1
+- Calculate proportionate screen height and width based on the device's screen size.
+- Easy to use and integrate into Flutter projects.
+- Helps in creating responsive layouts that adapt to different screen sizes.
+- Lightweight and efficient.
+- licenses added
+
+
---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_config/responsive_config.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ExampleScreen(),
+    );
+  }
+}
+
+class ExampleScreen extends StatelessWidget {
+  const ExampleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // You need to initialize SizeConfig before using it
+    ResponsiveConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Proportionate Height: ${getProportionateScreenHeight(100)}',
+            ),
+            Text(
+              'Proportionate Width: ${getProportionateScreenWidth(100)}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
