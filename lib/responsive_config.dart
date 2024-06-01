733a7364f26e392257e70d08f9d99fdaa2dcd82a
fawad8811 <108364741+fawad8811@users.noreply.github.com>
2024-06-01 13:02:43 -0700

    Update responsive_config.dart

--- lib/responsive_config.dart
+++ lib/responsive_config.dart
@@ -19,12 +19,12 @@ class ResponsiveConfig {
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = ResponsiveConfig.screenHeight;
   // 812 is the layout height that designer use
-  return (inputHeight / 812.0) * screenHeight;
+  return (inputHeight / 720.0) * screenHeight;
 }
 
 // Get the proportionate height as per screen size
 double getProportionateScreenWidth(double inputWidth) {
   double screenWidth = ResponsiveConfig.screenWidth;
   // 375 is the layout width that designer use
-  return (inputWidth / 375.0) * screenWidth;
+  return (inputWidth / 1360.0) * screenWidth;
 }
